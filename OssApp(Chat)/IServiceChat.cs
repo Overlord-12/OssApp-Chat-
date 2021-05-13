ca8bc91baad39b64c8f8f333e718419fcb9875ff
Overlord-12 <danya.argastesv.02@mail.ru>
2021-05-13 13:33:54 +0300

    Добавьте файлы проекта.

---
+++ OssApp(Chat)/IServiceChat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace OssApp_Chat_
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IServerChatCallBack))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        void DoWork();
+        [OperationContract]
+        int Connetct();
+        [OperationContract]
+        void Dissconect(int id);
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string str);
+    }
+
+    public interface IServerChatCallBack
+    {
+        [OperationContract]
+        void MsgCallBack(string str);
+
+    }
+    
+}
---
+++ OssApp(Chat)/ServiceChat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace OssApp_Chat_
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "ServiceChat" в коде и файле конфигурации.
+    public class ServiceChat : IServiceChat
+    {
+        public int Connetct()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dissconect(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoWork()
+        {
+        }
+
+        public void SendMessage(string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
